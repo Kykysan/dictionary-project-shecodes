7d564c23816ee6ab552462ee210389a891ae050b
Kykysan <76854419+Kykysan@users.noreply.github.com>
2021-04-11 11:23:06 -0700

    Create CSS file for dictionary

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import Dictionary from "./Dictionary";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <header className="App-header">Hello
+      <header className="App-header">
+        <Dictionary />
       </header>
       <footer>
         <a href="https://github.com/Kykysan/dictionary-project-shecodes" target="_blank" rel="noreferrer" id="git-hub">Open source code</a> by Kylee San Miguel
