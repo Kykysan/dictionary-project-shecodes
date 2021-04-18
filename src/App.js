1cdf978c0138af4c22778304067ca723d006ff5d
Kykysan <76854419+Kykysan@users.noreply.github.com>
2021-04-18 10:04:27 -0700

    Added H1 & H2 quote

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
+        <h1>Search The Universe</h1>
       <header className="App-header">
         <Dictionary />
       </header>
-      
+      <h2>"Somewhere, something incredible is waiting to be known"</h2>
+      <p>~Carl Sagan</p>
       <footer>
         <a href="https://github.com/Kykysan/dictionary-project-shecodes" target="_blank" rel="noreferrer">Open source code</a> by Kylee San Miguel
       </footer>
