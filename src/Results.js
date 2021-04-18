a9e1f98e820618253dd645399f21261ca1f5ef01
Kykysan <76854419+Kykysan@users.noreply.github.com>
2021-04-18 11:04:38 -0700

    Editing with size and color for design formatting

--- src/Results.js
+++ src/Results.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Meaning from "./Meaning";
 import Phonetic from "./Phonetic";
+import "./Results.css";
 
 
 export default function Results(props){
     if (props.results){
         return <div className="Results">
             <section>
-            <h2>{props.results.word}</h2>
+            <h3>{props.results.word}</h3>
             {props.results.phonetics.map(function(phonetic, index){
                 return(
                     <div key={index}>
@@ -26,5 +27,6 @@ export default function Results(props){
             </div>;
     } else {
         return null;
-    }    
+    }  
+          
 }
