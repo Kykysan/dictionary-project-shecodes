ed289606bd301dc0af6127f2d9c21b0c36656052
Kykysan <76854419+Kykysan@users.noreply.github.com>
2021-04-18 17:21:06 -0700

    Adding max width for app container

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
       <header className="App-header">
         <Dictionary />
       </header>
-      <h2>"Somewhere, something incredible is waiting to be known"</h2>
+  <h2>"Somewhere, something incredible is waiting to be known"</h2>
       <p>~Carl Sagan</p>
       <footer>
         <a href="https://github.com/Kykysan/dictionary-project-shecodes" target="_blank" rel="noreferrer">Open source code</a> by Kylee San Miguel
